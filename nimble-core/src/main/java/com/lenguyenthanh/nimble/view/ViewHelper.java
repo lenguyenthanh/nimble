8e0f1aba073e791404adccdd0ad97ab376f1d27f
Thanh Le <lenguyenthanh@hotmail.com>
2016-02-23 20:49:47 +0700

    Remove getActivity duplicate code

---
+++ nimble-core/src/main/java/com/lenguyenthanh/nimble/view/ViewHelper.java
@@ -0,0 +1,27 @@
+package com.lenguyenthanh.nimble.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+
+public class ViewHelper {
+  private ViewHelper() {
+  }
+
+  private static ViewHelper instance;
+
+  public static ViewHelper getInstance() {
+    return instance;
+  }
+
+  public Activity getActivity(Context context) {
+    while (!(context instanceof Activity) && context instanceof ContextWrapper) {
+      context = ((ContextWrapper) context).getBaseContext();
+    }
+    if (!(context instanceof Activity)) {
+      throw new IllegalStateException(
+          "Expected an activity context, got " + context.getClass().getSimpleName());
+    }
+    return (Activity) context;
+  }
+}
