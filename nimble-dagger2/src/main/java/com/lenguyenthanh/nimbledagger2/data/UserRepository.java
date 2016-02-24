3916c75dc9ce88738db0adbf491cdf90d56c836e
Thanh Le <lenguyenthanh@hotmail.com>
2016-02-24 21:18:33 +0700

    Implement fake UserRespository

---
+++ nimble-dagger2/src/main/java/com/lenguyenthanh/nimbledagger2/data/UserRepository.java
@@ -0,0 +1,7 @@
+package com.lenguyenthanh.nimbledagger2.data;
+
+import com.lenguyenthanh.nimbledagger2.data.model.User;
+
+public interface UserRepository {
+  User getUser();
+}
---
+++ nimble-dagger2/src/main/java/com/lenguyenthanh/nimbledagger2/data/model/User.java
@@ -0,0 +1,11 @@
+package com.lenguyenthanh.nimbledagger2.data.model;
+
+public class User {
+  public User(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  public String firstName;
+  public String lastName;
+}
